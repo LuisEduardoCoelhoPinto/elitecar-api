14acf238ece8dd3d24f7ae3d3ed55762fdbe108f
Fulano de Tal <fulanodetal@exemplo.br>
2025-11-18 14:20:00 -0300

    elitecar-api

--- src/model/Cliente.ts
+++ src/model/Cliente.ts
@@ -8,75 +8,82 @@ class Cliente {
     private nome: string;
     private cpf: string;
     private telefone: string;
+
 constructor(
     _nome: string,
     _cpf: string,
-    _telefone: string
+    _telefone: string,
 ) {
     this.nome = _nome;
     this.cpf = _cpf;
     this.telefone = _telefone;
 }
 
-    public getIdCliente(): number {
+public getIdCliente(): number {
     return this.idCliente;
 }
 
-    public setIdCliente(idCliente: number): void {
+public setIdCliente(idCliente: number): void {
     this.idCliente = idCliente;
 }
 
-    public getNome(): string {
+public getNome(): string {
     return this.nome;
 }
 
-    public setNome(nome: string): void {
+public setNome(nome: string): void {
     this.nome = nome;
 }
 
-    public getCpf(): string {
+public getCpf(): string {
     return this.cpf;
 }
 
-    public setCpf(cpf: string): void {
+public setCpf(cpf: string): void {
     this.cpf = cpf;
 }
 
-    public getTelefone(): string {
+public getTelefone(): string {
     return this.telefone;
 }
 
-    public setTelefone(telefone: string): void {
+public setTelefone(telefone: string): void {
     this.telefone = telefone;
 }
+
 static async listarClientes(): Promise<Array<Cliente> | null> {
 try {
     let listaDeClientes: Array<Cliente> = [];
+
     const querySelectClientes = `SELECT * FROM clientes;`;
+
     const respostaBD = await database.query(querySelectClientes);
 
-    respostaBD.rows.forEach((clienteBD) => {
+respostaBD.rows.forEach((clienteBD) => {
     const novoCliente: Cliente = new Cliente(
     clienteBD.nome,
     clienteBD.cpf,
     clienteBD.telefone
 );
+
     novoCliente.setIdCliente(clienteBD.id_cliente);
+
     listaDeClientes.push(novoCliente);
 });
 
     return listaDeClientes;
-    } catch (error) {
+} catch (error) {
     console.error(`Erro na consulta ao banco de dados. ${error}`);
     return null;
 }
 }
+
 static async cadastrarCliente(cliente: ClienteDTO): Promise<boolean> {
 try {
-    const queryInsertCliente = `INSERT INTO clientes (nome, cpf, telefone)
-    VALUES
-    ($1, $2, $3)
-    RETURNING id_cliente;`;
+const queryInsertCliente = `INSERT INTO clientes (nome, cpf, telefone)
+VALUES
+($1, $2, $3)
+RETURNING id_cliente;`;
 
 const respostaBD = await database.query(queryInsertCliente, [
     cliente.nome.toUpperCase(),
@@ -94,24 +101,30 @@ if (respostaBD.rows.length > 0) {
     return false;
     }
 }
+
 static async listarCliente(idCliente: number): Promise<Cliente | null> {
-try {
+    try {
     const querySelectCliente = `SELECT * FROM clientes WHERE id_cliente=$1;`;
+
     const respostaBD = await database.query(querySelectCliente, [idCliente]);
+
 if(respostaBD.rowCount != 0) {
     const cliente: Cliente = new Cliente(
     respostaBD.rows[0].nome,
     respostaBD.rows[0].cpf,
     respostaBD.rows[0].telefone
 );
-    cliente.setIdCliente(respostaBD.rows[0].id_cliente);
-    return cliente;
+
+cliente.setIdCliente(respostaBD.rows[0].id_cliente);
+return cliente;
 }
+
     return null;
 } catch (error) {
     console.error(`Erro ao buscar cliente no banco de dados. ${error}`);
     return null;
-    }
+}
+
 }
 }
 
